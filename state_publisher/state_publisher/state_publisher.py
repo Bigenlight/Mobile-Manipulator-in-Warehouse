5726a60aaf236adb34fed070968a00004dc52eec
Baek100-KR <100rudtjs@naver.com>
2024-11-20 14:22:21 +0900

    백승민 커밋

--- state_publisher/state_publisher/state_publisher.py
+++ state_publisher/state_publisher/state_publisher.py
@@ -12,16 +12,32 @@ class StatePublisher(Node):
         self.timer_period = 3  # 3초마다 퍼블리시
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
         self.state = 0
+        self.repeat_count = 0  # 4번 상태를 퍼블리시한 횟수
         self.get_logger().info('State Publisher가 시작되었습니다.')
 
     def timer_callback(self):
-        msg = Int32()
-        msg.data = self.state
-        self.publisher_.publish(msg)
-        self.get_logger().info(f'퍼블리시된 상태: {self.state}')
-        self.state += 1
+        # 4번 상태만 3번 퍼블리시
+        if self.state == 4:
+            if self.repeat_count < 3:
+                msg = Int32()
+                msg.data = self.state
+                self.publisher_.publish(msg)
+                self.get_logger().info(f'퍼블리시된 상태: {self.state}')
+                self.repeat_count += 1
+            else:
+                # 3번 퍼블리시 후 다음 상태로 넘어감
+                self.state += 1
+                self.repeat_count = 0
+        else:
+            msg = Int32()
+            msg.data = self.state
+            self.publisher_.publish(msg)
+            self.get_logger().info(f'퍼블리시된 상태: {self.state}')
+            self.state += 1
+
+        # 상태 초기화
         if self.state > 10:
-            self.state = 0  # 상태를 0으로 초기화하여 반복
+            self.state = 0
 
 def main(args=None):
     rclpy.init(args=args)
